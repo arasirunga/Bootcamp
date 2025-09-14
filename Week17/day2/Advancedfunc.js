694bbd199f33545a7ae13447c811c1dfe0402fb9
arasirunga <arasirunga@gmail.com>
2025-09-14 23:07:03 +0400

    week2

---
+++ Week17/day2/Advancedfunc.js
@@ -0,0 +1,13 @@
+const sum = (a, b) => a + b;
+
+
+function kilo (weight )
+{
+return weight *1000
+
+
+}
+
+kilo(2)
+//function declaration tells you what the function does. function invoking is when you call the function and
+//required values as arguments
