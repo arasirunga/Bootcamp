59252cf5bbdabab3a3332bbdb8fd383dd48b04c2
arasirunga <arasirunga@gmail.com>
2025-10-02 22:10:00 +0400

    week7, day2

---
+++ Week7/day2/daily.sql
@@ -0,0 +1,16 @@
+create actors  {
+    id serial primary key,
+    last_name varchar(50) not null,
+    first_name varchar(50) not null,
+    birth_date date not null
+};
+
+
+INSERT INTO actors (last_name, first_name, birth_date) VALUES
+('Marc', 'Benichou', '1998-11-02'),
+('Yoan', 'Cohen', '2010-12-03'),
+('Lea', 'Benichou', '1987-07-27'),
+('Amelia', 'Dux', '1996-04-07'),
+('David', 'Grez', '2003-06-14'),
+('Omer', 'Simpson', '1980-10-03'),
+('Rungassamy', 'Arasi', '2001-07-14');
